8e0fb0c62a7e54b70734a0ad3aaa0903d8236a26
Soniya Bisht <bishtsoniya216@gmail.com>
2026-07-23 18:41:44 +0530

    Initialize main class & first java prgrm

---
+++ First.java
@@ -0,0 +1,9 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+/*Java Program */
+/*Java is a class-based, high level object-oriented programming language.
+developed by Sun Microsystems
+This is a simple Java program, that prints "Hello, World!" */
---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        // This is my first Java Program.
+        /*
+        This
+        is
+        a
+        multi-line
+        comment.
+         */
+        // sout then tab ->System.out.println();
+        System.out.print("I like coffee.\n");
+        System.out.println("The grounded brewed one is really good! ");
+        System.out.println("I'll love to have cold coffee.");
+    }
+}
+
