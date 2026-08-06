4a7fe24f69a421864a6f509c0d59a7ae0c3a8c31
Soniya Bisht <bishtsoniya216@gmail.com>
2026-08-06 23:55:56 +0530

    HandShoot Game - rock paper scissors in java

--- HandShoot_game.java
+++ HandShoot_game.java
@@ -2,9 +2,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HandShoot_game {
-  /**
-   * @param args
-   */
   public static void main(String[] args){
     
     //ROCK PAPER SCISSORS GAME 
