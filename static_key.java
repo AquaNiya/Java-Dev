54443dc8cf418fbcfb88af5788d4184939bc65bf
Soniya Bisht <bishtsoniya216@gmail.com>
2026-08-13 00:06:25 +0530

    Static keyword in Java

---
+++ static_key.java
@@ -0,0 +1,19 @@
+public class static_key {
+  public static void main(String[] args) {
+    
+    //static keyword = makes a variable or method belong to the class
+    //                 rather than to any specific object.
+    //       Commonly used for utility methods or shared resources.
+
+    Friend friend1 = new Friend("Spongebob");
+    Friend friend2 = new Friend("Patrick");
+    Friend friend3 = new Friend("Squidward");
+    Friend friend4 = new Friend("Sandy");
+
+    System.out.println(Friend.numOfFriends);
+    Friend.showFriends();
+
+    //In \Math class there's static method - round
+    Math.round(3.99);
+  }
+}
