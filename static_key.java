c803bb161ae2a379a9acadd53c04f512587665e9
Soniya Bisht <bishtsoniya216@gmail.com>
2026-08-16 16:58:17 +0530

    Declaring the friends variable

--- static_key.java
+++ static_key.java
@@ -9,10 +9,15 @@ public class static_key {
     Friend friend2 = new Friend("Patrick");
     Friend friend3 = new Friend("Squidward");
     Friend friend4 = new Friend("Sandy");
-
-   
-
+    
+    System.out.println(friend1.name);
+    System.out.println(friend2.name);
+    System.out.println(friend3.name);
+    System.out.println(friend4.name);
+    
+    // Optimal way
     System.out.println(Friend.numOfFriends);
+    
     Friend.showFriends();
 
     //In \Math class there's static method - round
