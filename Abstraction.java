60d58cc807eeb00c67f7f517ba1ee31f18cbef61
Soniya Bisht <bishtsoniya216@gmail.com>
2026-09-01 23:18:56 +0530

    Restore Java ignore rules and remove compiled artifacts

---
+++ Abstraction.java
@@ -0,0 +1,15 @@
+public class Abstraction {
+  public static void main(String[] args){
+
+    //abstract =  used to define abstract classes and methods
+    //Abstraction is the process of hiding implementation details
+    //& showing only the essential features;
+    //Abstract classes can't be instantiated directly
+    //can contain 'abstract methods' (which must be implemented)
+    //can contain 'concrete' methods (which are inherited)
+
+
+    //Create different classes Shape, circle, triangle & rectangle
+  }
+
+}
